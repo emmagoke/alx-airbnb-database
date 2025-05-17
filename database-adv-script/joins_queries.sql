c6680421b9fdd4b900c15432b20d55183f244106
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-05-17 10:48:39 +0100

    feat: added the order by  to left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,6 +9,7 @@ ON booking.user_id = users.user_id
 SELECT * FROM properties
 LEFT JOIN review
 ON properties.property_id = review.property_id
+ORDER BY properties.property_id, review.review_id
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and
 -- all bookings, even if the user has no booking or a booking is not linked to a user.
