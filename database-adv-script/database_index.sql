62981ac35e3ce13e58a0d40a5df6ba8891416520
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-05-17 13:34:43 +0100

    feat: added the query that was used to analyse index

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,19 @@
+EXPLAIN ANALYZE
+SELECT
+    p.property_id,
+    p.name AS property_name,
+    p.description AS property_description,
+    par.average_rating
+FROM
+    properties p
+INNER JOIN
+    (SELECT property_id, AVG(rating) AS average_rating FROM review GROUP BY property_id) par
+    ON p.property_id = par.property_id
+WHERE
+    par.average_rating > 4.0 AND p.name LIKE 'Cozy%' -- Added a filter on p.name
+ORDER BY
+    par.average_rating DESC, p.property_id;
+
 -- Create Index
 CREATE INDEX IF NOT EXISTS idx_property_name ON properties (name);
 CREATE INDEX IF NOT EXISTS idx_property_price_per_night ON properties (price_per_night);
